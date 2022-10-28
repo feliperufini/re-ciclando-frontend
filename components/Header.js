8f144d452130a01047d4616eb42822b7446d3477
Felipe Andrade <feliperufini01@gmail.com>
2022-10-28 01:05:10 -0400

    [feat] - started homepage

--- components/Header.js
+++
@@ -1,5 +0,0 @@
-export function Header() {
-  return(
-    <h1>Página Header</h1>
-  );
-}
--- pages/home/index.js
+++ pages/home/index.js
@@ -1,5 +1,22 @@
+import DesktopMenu from "../../components/DesktopMenu";
+import HeaderNavbar from "../../components/HeaderNavbar";
+import MobileMenu from "../../components/MobileMenu";
+
 export default function Home() {
-  return(
-    <h1>Página Home</h1>
+  return (
+    <div className="flex h-screen bg-emerald-100 dark:bg-gray-900">
+      <DesktopMenu />
+      {/* <MobileMenu /> */}
+      <div className="flex flex-col flex-1">
+        <HeaderNavbar />
+        {/* <main className="h-full pb-16 overflow-y-auto">
+          <div className="container px-6 mx-auto grid">
+            <h2 className="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">
+              Blank
+            </h2>
+          </div>
+        </main> */}
+      </div >
+    </div >
   );
 }
